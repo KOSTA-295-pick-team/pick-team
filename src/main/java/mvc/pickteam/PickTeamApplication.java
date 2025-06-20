80106188ea33a1abe699d8a977183dc3828da72f
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-06-20 19:27:47 +0900

    Spring-Boot 디폴트 세팅
    maven 의존성, application.properties 설정 등 초기 세팅
    (.env 파일은 개인별로 별도 추가 혹은 외부 주입)
    Spring 프로젝트가 정상적으로 기동되는 것을 확인 후 커밋

---
+++ src/main/java/mvc/pickteam/PickTeamApplication.java
@@ -0,0 +1,13 @@
+package mvc.pickteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mvc/pickteam/PickTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.pickteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

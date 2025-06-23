f9228b3a738340be14c8a0d62cc46a201089d661
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-06-23 18:19:04 +0900

    추가, 수정, 삭제 관련 공통 기능
    src\main\java\com\pickteam\domain\common\BaseSoftDeleteByAnnotation.java - @Softdelete 어노테이션을 이용한 방식으로 soft-delete를 구현하도록 공통 컬럼 및 메소드 정의
    src\main\java\com\pickteam\domain\common\BaseSoftDeleteSupportEntity.java - 수동으로 컨트롤하는 방식으로 soft-delete를 구현하도록 공통 컬럼 및 메소드 정의
    src\main\java\com\pickteam\domain\common\BaseTimeEntity.java - CreatedAt, UpdatedAt을 기록하는 공통 컬럼 및 메소드 정

---
+++ src/main/java/com/pickteam/domain/common/BaseSoftDeleteSupportEntity.java
@@ -0,0 +1,35 @@
+package com.pickteam.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Soft-Delete를 수동 관리하는 코드
+ */
+@Getter
+@MappedSuperclass
+public abstract class BaseSoftDeleteSupportEntity extends BaseTimeEntity {
+
+    @Column(name="is_deleted", nullable = false)
+    protected Boolean isDeleted = false;
+
+    @Column(name="deleted_at")
+    protected LocalDateTime deletedAt;
+
+    public void markDeleted() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void restore() {
+        this.isDeleted = false;
+        this.deletedAt = null;
+    }
+
+    public boolean isActive() {
+        return !Boolean.TRUE.equals(this.isDeleted);
+    }
+}

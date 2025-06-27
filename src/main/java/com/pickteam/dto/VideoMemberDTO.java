f00be02cda4f27658897e419ec67ff8c30d72135
blueberrylatte <yoffle0987@gmail.com>
2025-06-27 17:37:00 +0900

    화상회의채널 crud

---
+++ src/main/java/com/pickteam/dto/VideoMemberDTO.java
@@ -0,0 +1,40 @@
+package com.pickteam.dto;
+
+import com.pickteam.domain.common.BaseSoftDeleteSupportEntity;
+import com.pickteam.domain.enums.UserRole;
+import com.pickteam.domain.user.Account;
+import com.pickteam.domain.videochat.VideoChannel;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoMemberDTO {
+
+    private Long id;
+
+    private Long userId;
+
+    private String email;
+
+    private String name;
+
+    private String mbti;
+
+    private String disposition;
+
+    private String introduction;
+
+    private String preferWorkstyle;
+
+    private String dislikeWorkstyle;
+
+    private LocalDateTime joinDate;
+
+}
---
+++ src/main/java/com/pickteam/service/VideoConferenceServiceImpl.java
@@ -0,0 +1,108 @@
+package com.pickteam.service;
+
+import com.pickteam.domain.user.Account;
+import com.pickteam.domain.videochat.VideoChannel;
+import com.pickteam.domain.videochat.VideoMember;
+import com.pickteam.domain.workspace.Workspace;
+import com.pickteam.dto.VideoChannelDTO;
+import com.pickteam.dto.VideoMemberDTO;
+import com.pickteam.exception.ErrorCode;
+import com.pickteam.exception.VideoConferenceException;
+import com.pickteam.repository.VideoChannelRepository;
+import com.pickteam.repository.VideoMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class VideoConferenceServiceImpl implements VideoConferenceService {
+
+
+    private final VideoChannelRepository videoChannelRepository;
+
+    private final VideoMemberRepository videoMemberRepository;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<VideoChannelDTO> selectVideoChannels(Long workspaceId, Long accountId) throws VideoConferenceException {
+        List<VideoChannel> channels = new ArrayList<>();
+        if (accountId == null) {
+            channels = videoChannelRepository.selectChannelsByWorkSpaceId(workspaceId);
+        } else {
+            channels = videoChannelRepository.selectChannelsByWorkSpaceId(workspaceId, accountId);
+        }
+        if (channels.isEmpty()) {
+            throw new VideoConferenceException(ErrorCode.CHANNELS_NOT_FOUND);
+        }
+
+        return channels.stream().map(channel -> modelMapper.map(channel, VideoChannelDTO.class)).toList();
+
+    }
+
+    @Transactional
+    @Override
+    public void insertVideoChannel(Long workspaceId, String videoChannelName) {
+        Workspace workspace = Workspace.builder().id(workspaceId).build();
+
+        videoChannelRepository.save(VideoChannel.builder().name(videoChannelName).workspace(workspace).build());
+
+    }
+
+    @Transactional
+    @Override
+    public void joinVideoChannel(Long accountId, Long videoChannelId) {
+
+        videoMemberRepository.save(VideoMember.builder().account(Account.builder().id(accountId).build()).
+                videoChannel(VideoChannel.builder().id(videoChannelId).id(videoChannelId).build()).build());
+
+    }
+
+    @Transactional
+    @Override
+    public void deleteVideoChannel(Long videoChannelId) throws VideoConferenceException {
+
+        VideoChannel channel = videoChannelRepository.findById(videoChannelId).orElseThrow(() -> new VideoConferenceException(ErrorCode.CHANNEL_NOT_FOUND));
+
+        videoChannelRepository.delete(channel);
+
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<VideoMemberDTO> selectVideoChannelParticipants(Long videoChannelId) throws VideoConferenceException {
+
+        List<VideoMember> members = videoMemberRepository.selectAccountsByChannelId(videoChannelId);
+
+        if (members.isEmpty()) {
+            throw new VideoConferenceException(ErrorCode.MEMBERS_NOT_FOUND);
+        }
+
+        return members.stream().map(member -> {
+            VideoMemberDTO memberDTO = modelMapper.map(member.getAccount(), VideoMemberDTO.class);
+            memberDTO.setJoinDate(member.getCreatedAt());
+            memberDTO.setUserId(member.getAccount().getId());
+            return memberDTO;
+        }).collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Override
+    public void deleteVideoChannelParticipant(Long memberId) throws VideoConferenceException {
+
+      VideoMember member = videoMemberRepository.findById(memberId).orElseThrow(()->new VideoConferenceException(ErrorCode.MEMBER_NOT_FOUND));
+
+      videoMemberRepository.delete(member);
+
+    }
+}

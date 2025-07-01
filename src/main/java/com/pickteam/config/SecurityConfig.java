d4a16b441dd6f8f584ab1a61533524b1a89d1fe1
blueberrylatte <yoffle0987@gmail.com>
2025-07-01 12:06:13 +0900

    실시간채팅(websocket) 기능 구현(인증,인가 처리 포함)_수정1

--- src/main/java/com/pickteam/config/SecurityConfig.java
+++ src/main/java/com/pickteam/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            //.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(authorize -> authorize
                 .anyRequest().permitAll()
             )

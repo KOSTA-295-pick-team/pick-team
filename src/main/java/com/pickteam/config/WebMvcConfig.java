90866660a3f421814a42bf18a16f889356911453
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-07-02 10:40:17 +0900

    환경변수 변경 누락 수정

--- src/main/java/com/pickteam/config/WebMvcConfig.java
+++ src/main/java/com/pickteam/config/WebMvcConfig.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 /**
+ * *** 파일 업로드 기능에 대한 중복 구현으로
+ * 선행 구현체를 사용하기로 합의함.
+ * 구현 방식이 달라 사용된 연관 클래스들을 남겨둠 ***
+ *
  * 웹 MVC 설정
  * - 정적 리소스 핸들링 설정
  * - 업로드된 파일 서빙 설정
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Value("${app.file.upload-dir}")
+    @Value("${app.profile.image-dir}") // 필요 시 환경변수 교체
     private String uploadDir;
 
     @Override

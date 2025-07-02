05f43388927626b0b5b19dbb446be52fb7b7ce7d
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-07-02 15:36:10 +0900

    파라미터명 통일 (accountId)

--- src/main/java/com/pickteam/service/team/TeamMemberService.java
+++ src/main/java/com/pickteam/service/team/TeamMemberService.java
@@ -1,28 +1,20 @@
 package com.pickteam.service.team;
 
-import com.pickteam.domain.team.TeamMember;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
 public interface TeamMemberService {
-    /** 팀 멤버 추가* */
-    public void addMember(Long teamId, Long accountId);
+    /** 팀 멤버 추가*/
+    void addMember(Long teamId, Long accountId);
+
+    /** 팀 멤버 삭제*/
+    void deleteMember(Long teamId, Long accountId);
 
     /** 팀 멤버 추방 (추방된 팀원은 차단 상태가 아니면 다시 팀에 들어올 수 있다.)
      * 추방과 관련된 추가 로직이 붙을 수 있으므로 메소드 분리 (내부적으로 soft-delete 메소드 호출)
      * */
-    public void banMember(Long teamId, Long memberId);
+    void banMember(Long teamId, Long accountId);
 
-    /** 팀 멤버 삭제 (해당 멤버를 soft-delete처리) */
-    public void deleteMember(Long teamId, Long memberId);
+    /** 팀 멤버 차단 (재가입 불가능 처리)*/
+    void blockMember(Long teamId, Long accountId);
 
-    /** 팀 멤버 차단*/
-    public void blockMember(Long teamId, Long memberId);
-
-    /** 팀 멤버 차단 해제*/
-    public void unblockMember(Long teamId, Long memberId);
-
-
+    /** 팀 멤버 차단 해제 */
+    void unblockMember(Long teamId, Long accountId);
 }

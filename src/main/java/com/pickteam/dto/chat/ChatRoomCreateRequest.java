3e1bd7b36193ede7660980bd4e9a688c89d1ae3b
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-07-04 19:25:19 +0900

    채팅방 DTO Request, Response

---
+++ src/main/java/com/pickteam/dto/chat/ChatRoomCreateRequest.java
@@ -0,0 +1,27 @@
+// ChatRoomCreateRequest.java
+package com.pickteam.dto.chat;
+
+import com.pickteam.domain.enums.ChatRoomType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ChatRoomCreateRequest {
+    @NotNull(message = "워크스페이스 ID는 필수입니다.")
+    private Long workspaceId;
+
+    @NotBlank(message = "채팅방 이름은 필수입니다.")
+    private String name;
+
+    @NotNull(message = "채팅방 타입은 필수입니다.")
+    private ChatRoomType type;
+
+    @NotNull(message="채팅에 참여한 멤버가 없습니다.")
+    private List<Long> ChatMemberIdLists;
+
+}

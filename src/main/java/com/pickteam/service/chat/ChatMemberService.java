c572215499379ca67ec549f1c350533925aa594b
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-07-08 20:56:45 +0900

    chatMemberService 베이스 구현
    (채팅방 입장, 퇴장, 마지막에 읽은 메시지 갱신, 특정 유저가 속한 채팅방 조회, 채팅방 멤버 조회)

--- src/main/java/com/pickteam/service/chat/ChatMemberService.java
+++ src/main/java/com/pickteam/service/chat/ChatMemberService.java
@@ -2,34 +2,34 @@ package com.pickteam.service.chat;
 
 import com.pickteam.domain.chat.ChatMember;
 import com.pickteam.domain.chat.ChatRoom;
-import com.pickteam.domain.chat.ChatMessage;
-import com.pickteam.domain.user.Account;
+import com.pickteam.dto.chat.ChatMemberResponse;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 public interface ChatMemberService {
     /**
      * 특정 채팅방에 사용자를 입장시킵니다.
-     * @param chatRoomId 입장할 채팅방 ID
      * @param accountId 입장할 사용자 ID
      * @return 생성된 ChatMember 객체
      */
-    ChatMember joinChatRoom(Long chatRoomId, Long accountId);
+    ChatMember joinChatRoom(Long accountId, Long workspaceId, Long chatId);
 
     /**
      * 특정 채팅방에서 사용자를 나가게 합니다.
      * @param chatRoomId 채팅방 ID
      * @param accountId 나갈 사용자 ID
      */
-    void leaveChatRoom(Long chatRoomId, Long accountId);
+    void leaveChatRoom(Long workspaceId, Long chatRoomId, Long accountId);
 
     /**
      * 특정 채팅방의 멤버 상세정보를 조회합니다.
+     *
      * @param chatRoomId 채팅방 ID
-     * @param accountId 조회할 멤버 ID
+     * @param accountId  조회할 멤버 ID
      * @return ChatMember 상세 정보
      */
-    ChatMember getChatMemberDetail(Long chatRoomId, Long accountId);
+    List<ChatMemberResponse> getChatMemberDetail(Long chatRoomId, Long accountId);
 
     /**
      * 특정 채팅방에서 멤버를 강제로 내보냅니다.
@@ -45,7 +45,7 @@ public interface ChatMemberService {
      * @param accountId 사용자 ID
      * @param messageId 마지막으로 읽은 메시지 ID
      */
-    void updateLastReadMessage(Long chatRoomId, Long accountId, Long messageId);
+    void updateLastReadMessage(Long workspaceId, Long chatRoomId, Long accountId, Long messageId);
 
     /**
      * 특정 사용자가 참여 중인 모든 채팅방 목록을 조회합니다.

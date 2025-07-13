a2ff9c64716caa3a318f5c3315a7e1d69359cb17
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-07-14 03:30:58 +0900

    Request DTO에 누락된 기본생성자 추가

--- src/main/java/com/pickteam/dto/chat/ChatRoomUpdateTitleRequest.java
+++ src/main/java/com/pickteam/dto/chat/ChatRoomUpdateTitleRequest.java
@@ -4,10 +4,15 @@ import com.pickteam.domain.enums.ChatRoomType;
 import jakarta.mail.Message;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoomUpdateTitleRequest {
 
     @NotBlank(message = "채팅방 이름은 필수입니다.")
